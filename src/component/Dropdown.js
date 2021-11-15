ff0b0b837d7a52e2bf2a1fbc6d5e96bf498e8841
inseok1121 <inseck1121@gmail.com>
2021-11-15 22:22:35 +0900

    Update Dropdown Component Hover

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -1,3 +1,4 @@
+import './Dropdown.css';
 const { Component } = require("react");
 
 class Dropdown extends Component{
@@ -6,43 +7,45 @@ class Dropdown extends Component{
 
         this.state ={
             showMenu: false,
+            selectedItem: "Select",
         }
 
-        this.handleClick = this.handleClick.bind(this);
+        this.handleHover = this.handleHover.bind(this);
         this.selectedItem = this.selectedItem.bind(this);
     }
 
-    handleClick(){
+    handleHover(){
         this.setState({
             showMenu:true,
         })
     }
 
-    selectedItem(){
+    selectedItem(param){
         this.setState({
             showMenu:false,
+            selectedItem:param,
         })
     }
 
     render() {
         return (
-             <div>
-                 <div onClick={this.handleClick}>
-                     Show Menu
-                 </div>
-                 
-                 {
-                     this.state.showMenu ? (
-                        <div className="Menu">
-                        <div onClick={this.selectedItem}> Menu Item 1</div>
-                        <div onClick={this.selectedItem}> Menu Item 2</div>
-                        <div onClick={this.selectedItem}> Menu Item 3</div>
-                     </div>
-                     ) :
-                     (
-                         null
-                     )
-                 }
+             <div className="dropdown">
+                <div className="dropdown-header" onMouseOver={this.handleHover}>
+                    {this.state.selectedItem}
+                </div>
+
+                {
+                    this.state.showMenu ? (
+                        <div className="dropdown-content">
+                            <div className="dropdown-detail" onClick={this.selectedItem.bind(this, "Menu Item 1")}> Menu Item 1</div>
+                            <div className="dropdown-detail" onClick={this.selectedItem.bind(this, "Menu Item 2")}> Menu Item 2</div>
+                            <div className="dropdown-detail" onClick={this.selectedItem.bind(this, "Menu Item 3")}> Menu Item 3</div>
+                        </div>
+                    ) : (
+                        null
+                    )
+                }
+
 
 
              </div>
